11f8b6582e396640dbf86cc34cdfe1e2dec59bdd
Dwi Candra Permana <dwichan@outlook.com>
2020-01-21 03:24:43 +0700

    Initial commit

---
+++ PresensiRFID_CSharp/frmMain.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace ArduinoTest {
+    partial class frmMain {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent () {
+            this.components = new System.ComponentModel.Container();
+            this.btnConfig = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.txtOut = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cmbMode = new System.Windows.Forms.ComboBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.tmrSearch = new System.Windows.Forms.Timer(this.components);
+            this.tmrComm = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // btnConfig
+            // 
+            this.btnConfig.Location = new System.Drawing.Point(12, 12);
+            this.btnConfig.Name = "btnConfig";
+            this.btnConfig.Size = new System.Drawing.Size(113, 37);
+            this.btnConfig.TabIndex = 1;
+            this.btnConfig.Text = "Konfigurasi";
+            this.btnConfig.UseVisualStyleBackColor = true;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConnect.Location = new System.Drawing.Point(500, 12);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 37);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDisconnect.Enabled = false;
+            this.btnDisconnect.Location = new System.Drawing.Point(581, 12);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(83, 37);
+            this.btnDisconnect.TabIndex = 3;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // txtOut
+            // 
+            this.txtOut.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtOut.BackColor = System.Drawing.Color.Silver;
+            this.txtOut.Font = new System.Drawing.Font("Consolas", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOut.Location = new System.Drawing.Point(12, 55);
+            this.txtOut.Multiline = true;
+            this.txtOut.Name = "txtOut";
+            this.txtOut.ReadOnly = true;
+            this.txtOut.Size = new System.Drawing.Size(652, 320);
+            this.txtOut.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(131, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Mode:";
+            // 
+            // cmbMode
+            // 
+            this.cmbMode.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbMode.FormattingEnabled = true;
+            this.cmbMode.Items.AddRange(new object[] {
+            "PRESENSI",
+            "TAMBAH KARTU",
+            "HAPUS KARTU"});
+            this.cmbMode.Location = new System.Drawing.Point(134, 28);
+            this.cmbMode.Name = "cmbMode";
+            this.cmbMode.Size = new System.Drawing.Size(126, 21);
+            this.cmbMode.TabIndex = 8;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.Location = new System.Drawing.Point(581, 381);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(83, 28);
+            this.btnClose.TabIndex = 9;
+            this.btnClose.Text = "Tutup";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // tmrSearch
+            // 
+            this.tmrSearch.Interval = 10;
+            this.tmrSearch.Tick += new System.EventHandler(this.tmrSearch_Tick);
+            // 
+            // tmrComm
+            // 
+            this.tmrComm.Interval = 1000;
+            this.tmrComm.Tick += new System.EventHandler(this.tmrComm_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 421);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.cmbMode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtOut);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnConfig);
+            this.Name = "frmMain";
+            this.Text = "Sistem Presensi Mahasiswa - SERVER";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConfig;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.TextBox txtOut;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbMode;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Timer tmrSearch;
+        private System.Windows.Forms.Timer tmrComm;
+    }
+}
+
---
+++ PresensiRFID_CSharp/frmMain.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO.Ports;
+using System.Threading;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace ArduinoTest {
+    public partial class frmMain : Form {
+        private SerialPort serialPort = new SerialPort();
+        private string portUsed = "";
+
+        public frmMain () {
+            InitializeComponent();
+        }
+
+        private bool AutodetectArduinoPort () {
+            SerialPort tmp;
+            foreach (string str in SerialPort.GetPortNames())
+            {
+                tmp = new SerialPort(str);
+                if (tmp.IsOpen == false)
+                {
+                    try
+                    {
+                        //open serial port
+                        serialPort.PortName = str;
+                        serialPort.Open();
+                        serialPort.BaudRate = 9600;
+                        serialPort.WriteTimeout = 10;
+                        serialPort.ReadTimeout = 10;
+                        serialPort.Write(str);
+                        txtOut.Text += "\r\nPerangkat terhubung ke komputer via Komunikasi Serial dengan Port " + str + "!";
+                        portUsed = str;
+                        tmrSearch.Enabled = true;
+                        tmrComm.Enabled = false;
+                        return true;
+                    }
+                    catch (TimeoutException)
+                    {
+                        serialPort.Close();
+                        return false;
+                    }
+                    catch (Exception ex)
+                    {
+                        tmrComm.Enabled = false;
+                        MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private void btnConnect_Click (object sender, EventArgs e) {
+            if (btnConnect.Text == "Stop") {
+                tmrComm.Enabled = false;
+                btnConnect.Text = "Connect";
+            } else {
+                if (cmbMode.SelectedItem == null)
+                    MessageBox.Show(this, "Pilih mode yang akan digunakan!", "Choose Mode", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                else {
+                    txtOut.Text = "Mencari port yang akan digunakan...";
+                    tmrSearch.Enabled = false;
+                    tmrComm.Enabled = true;
+                    btnConnect.Text = "Stop";
+                }
+            }
+            
+        }
+
+        private void tmrComm_Tick (object sender, EventArgs e) {
+            if (AutodetectArduinoPort()) {
+                //serialPort.Write(portUsed);
+                btnDisconnect.Enabled = true;
+                btnConnect.Text = "Connect";
+                btnConnect.Enabled = false;
+            } 
+        }
+
+        private void btnDisconnect_Click (object sender, EventArgs e) {
+            try {
+                serialPort.Write("Disconnected.");
+                serialPort.Close();
+            } catch (Exception ex) {
+
+            } finally {
+                btnDisconnect.Enabled = false;
+                btnConnect.Enabled = true;
+                tmrSearch.Enabled = false;
+                tmrComm.Enabled = false;
+                portUsed = "";
+                txtOut.Text += "\r\nTerputus.";
+            }
+        }
+
+        private void btnClose_Click (object sender, EventArgs e) {
+            this.Close();
+        }
+
+        private void tmrSearch_Tick (object sender, EventArgs e) {
+            String s = serialPort.ReadExisting();
+            if (s != "") {
+                txtOut.Text += "\r\nDevice ID " + s + " terdeteksi! Mencari ketersediaan perangkat...";
+                //string json = "{\"code\":\"404\",\"result\":\"Device 1\"}";
+                var json = "";
+                using (WebClient wc = new WebClient()) {
+                    json = wc.DownloadString("http://localhost/PresensiRFID_Web/devs/cek_dev.json?device_id=" + s);
+                }
+                serialPort.Write(json);
+                dynamic ss = JsonConvert.DeserializeObject(json);
+                if (ss.code == 200) {
+                    txtOut.Text += "\r\nDevice ID " + s + " terdaftar dengan nama " + ss.result + "!";
+                } else if (ss.code == 404) {
+                    txtOut.Text += "\r\nDevice ID " + s + " tidak terdaftar!";
+                } else {
+                    txtOut.Text += "\r\nERROR!";
+                }
+            }
+        }
+    }
+}
